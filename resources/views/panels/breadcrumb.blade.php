5fc4e573711db329b60172d53bee6f84f91c3543
bimovl <bimo1valerian@gmail.com>
2022-11-24 15:03:49 +0700

    fixing

--- resources/views/panels/breadcrumb.blade.php
+++ resources/views/panels/breadcrumb.blade.php
@@ -19,7 +19,7 @@
         @endif
       </div>
       <div class="col s2 m6 l6">
-        <a class="btn dropdown-settings waves-effect waves-light breadcrumbs-btn right" href="#!"
+        {{-- <a class="btn dropdown-settings waves-effect waves-light breadcrumbs-btn right" href="#!"
           data-target="dropdown1">
           <i class="material-icons hide-on-med-and-up">settings</i>
           <span class="hide-on-small-onl">Settings</span>
@@ -32,7 +32,7 @@
           <li tabindex="0"><a class="grey-text text-darken-2" href="{{asset('page-faq')}}">FAQ</a></li>
           <li class="divider" tabindex="-1"></li>
           <li tabindex="0"><a class="grey-text text-darken-2" href="{{asset('user-login')}}">Logout</a></li>
-        </ul>
+        </ul> --}}
       </div>
     </div>
   </div>
